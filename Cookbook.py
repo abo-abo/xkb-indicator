c443bfd8a8eb101ae9af7fa1f832963c24c48101
Oleh Krehel <ohwoeowho@gmail.com>
2020-12-21 14:35:59 +0100

    Cookbook.py (setup_semimap): Add

--- Cookbook.py
+++ Cookbook.py
@@ -3,6 +3,9 @@ from pycook.recipes.pip import reinstall, publish
 import pycook.insta as st
 
 #* Recipes
+def setup_semimap(recipe):
+    st.cp("data/xmodmap.xkb", "/usr/share/X11/xkb/symbols/xx")
+
 def setup_desktop(recipe):
     if st.cp("data/xkbi.desktop", "~/.local/share/applications/xkbi.desktop"):
         st.bash("update-desktop-database ~/.local/share/applications/")
